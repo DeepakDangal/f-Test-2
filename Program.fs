1e6a15d6a88d481b88412008216c60a505733940
Deepak Dangal <deepak@example.com>
2023-11-29 08:48:18 -0500

    second commit

--- Program.fs
+++ Program.fs
@@ -1,1 +1,24 @@
-﻿
+﻿                //Test-2
+type Genre =
+    | Horror
+    | Drama
+    | Thriller
+    | Comedy
+    | Fantasy
+    | Sport
+
+
+type Director = {
+    Name: string
+    Movies: int
+}
+
+
+type Movie = {
+    Name: string
+    RunLength: int
+    Genre: Genre
+    Director: Director
+    IMDBRating: float
+}
+
